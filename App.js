b236222cd176728db71f554ba627758bc38401ad
Vishal Gupta <90566964+guptav100@users.noreply.github.com>
2023-11-08 18:24:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-// ## Namaste React Course by Akshay Saini
-// Chapter 09 - Optimizing our App
+
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -12,27 +11,9 @@ import Contact from "./Components/Contact";
 import Login from "./Components/Login";
 import RestaurantMenu from "./Components/RestaurantMenu";
 import Profile from "./Components/ProfileClass";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; // for routing our page import createBrowserRouter and RouterProvider for providing router & Outlet for children component for nested routing
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; 
+                      
 
-/* My Food App structure will look like this, 
-            1) Header
-                - Logo
-                - Nav Items(right side)
-                - Cart
-            2) Body
-                - Search bar
-                - Restaurants List
-                    - Restaurant card
-                        - Image
-                        - Name
-                        - Rating
-            3) Footer
-                - Links
-                - Copyrights
-       
-*/
-
-// AppLayout component to render: Header, Outlet(it contain children component like body, About, Restaurant Menu etc) and Footer Component
 const AppLayout = () => {
   return (
     <React.Fragment>
@@ -43,14 +24,12 @@ const AppLayout = () => {
   );
 };
 
-// call createBrowserRouter for routing different pages
 const appRouter = createBrowserRouter([
   {
     path: "/", // show path for routing
-    element: <AppLayout />, // show component for particular path
-    errorElement: <Error />, // show error component for path is different
+    element: <AppLayout />,
+    errorElement: <Error />, 
     children: [
-      // show children component for routing
       {
         path: "/",
         element: <Body />,
@@ -58,7 +37,7 @@ const appRouter = createBrowserRouter([
       {
         path: "about",
         element: <About />,
-        children: [{ // nested routing
+        children: [{ 
           path: "profile",
           element: <Profile />,
         }]
@@ -79,4 +58,4 @@ const appRouter = createBrowserRouter([
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} />); // render RouterProvider and use router as props and pass value appRouter
+root.render(<RouterProvider router={appRouter} />); 
